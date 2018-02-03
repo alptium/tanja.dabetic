efd49b01012464009916e870c42dc2c23c7a5b70
tanjadab <dabetictanja@gmail.com>
2018-02-03 12:34:01 +0100

    fixed

--- calculator/src/tanjadab/calculator/Main.java
+++ calculator/src/tanjadab/calculator/Main.java
@@ -1,22 +1,15 @@
 package tanjadab.calculator;
-
 import java.util.Scanner;
-
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		int num1, num2;
 		int result = 0;
-		String operation;
-		
+		String operation;	
 		try(Scanner sc = new Scanner(System.in)) {
-
 			System.out.println("Enter your first number:");
-			num1 = sc.nextInt();
-			
+			num1 = sc.nextInt();			
 			System.out.println("Choose your operation +, -, * ,/ ");
-			operation = sc.next();
-			
+			operation = sc.next();		
 			System.out.println("Enter your second number:");
 			num2 = sc.nextInt();
 			
@@ -28,10 +21,8 @@ public class Main {
 				result = num1 * num2;		
 			} else if(operation.equals("/")) {
 				result = num1 / num2;			
-			}
-			
+			}		
 			System.out.println(num1 + operation + num2 + " = " + result);
 		}
 	}
-
 }
--- hello_world/src/tanjadab/helloworld/Main.java
+++ hello_world/src/tanjadab/helloworld/Main.java
@@ -1,12 +1,8 @@
 package tanjadab.helloworld;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("Hello World!");
 		System.out.println("My name is Tanja.");
-
 	}
-
 }
